76b3cb046c2553c754061a58b03738433b5b6f90
Gizmo675 <41105943+Gizmo675@users.noreply.github.com>
2021-10-06 12:23:41 +0200

    resolve focous out probleme with onblur

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Select from 'react-select'
 
@@ -7,10 +7,6 @@ import Dougs from './Assets/dougs.svg'
 import Calcul from './Assets/calculator-solid.svg'
 
 function App() {
-
-  useEffect(() => {
-
-  })
 
   const [taux, setTaux] = useState('') 
   const [ttc, setTtc] = useState('') 
@@ -67,6 +63,19 @@ function App() {
     setChampTaux(true)
   }
 
+  const calculHT = () => {
+    const resultHT = ( ttc / (1+( taux/100 )) )
+    if(ht !== null){
+      setHt((resultHT).toFixed(2))
+    }
+  }
+  const calculTTC = () => {
+    const resultTTC = ( ht * (1 + (taux/100)) )
+    if (ttc !== null){
+      setHt((resultTTC).toFixed(2))
+    }
+  }
+
   return (
     <div className="App">
         <header className="App-header">
@@ -96,43 +105,64 @@ function App() {
               }
 
           </div>
+
             <div id="ttc">
-                <label htmlFor='ttc' className='label'>
-                  Montant TTC :
-                  <input 
-                    name='ttc'
-                    type='text'
-                    placeholder='TTC'
-                    value={ttc}
-                    // readOnly={ht ? true : false}
-                    onChange={(e) => setTtc((e.target.value) || 0)}
-                    onFocus={handleKey}
-                  />
-                </label> 
+              <label htmlFor='ttc' className='label'>
+                Montant TTC :
+                {ht ? 
+                <input 
+                  name='ttc'
+                  type='text'
+                  placeholder='TTC'
+                  value={ttc}
+                  readOnly
+                />
+                :
+                <input 
+                  name='ttc'
+                  type='text'
+                  placeholder='TTC'
+                  value={ttc}
+                  onChange={(e) => setTtc((e.target.value) || 0)}
+                  onFocus={handleKey}
+                  onBlur={calculHT}
+                />                  
+              }
+              </label>
+            </div>
 
-              <div>
-                <div id='ht'>
-                  <label htmlFor='ht' className='label'>
-                    Montant HT :
-                    <input 
-                      name='ht'
-                      type='text'
-                      placeholder='HT'
-                      readOnly={ttc ? true : false}
-                      value={ht}
-                      onChange={(e) => setHt((e.target.value) || 0)}
-                    />
-                  </label>
+            <div id='ht'>
+            <label htmlFor='ht' className='label'>
+                Montant HT :
+              {ttc ?
+              <input 
+                name='ht'
+                type='text'
+                placeholder='HT'
+                readOnly
+                value={ht}
+              />
+              :                 
+              <input 
+                name='ht'
+                type='text'
+                placeholder='HT'
+                value={ht}
+                onChange={(e) => setHt((e.target.value) || 0)}
+                onBlur={calculTTC}
+              />}
+              </label>
 
-                  {champ ? '' : 
-                  <div>
-                    <p className='warning'>Merci de renseigner un montant HT ou TTC</p>
-                  </div>}
+            </div>
 
-                </div>
-                <div className="label"><p>Montant de la TVA :</p><p>{tva}</p></div>
-              </div>
-          </div>
+            {champ ? '' : 
+            <div>
+              <p className='warning'>Merci de renseigner un montant HT ou TTC</p>
+            </div>}
+
+            <div className="label"><p>Montant de la TVA :</p><p>{tva}</p></div>
+              
+
 
         </div>
           <div className="button_undo">
